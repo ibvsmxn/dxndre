c5f4717842a8c03b4cc631d6b839276bdae22c4c
D'André Phillips <busman4@hotmail.co.uk>
2019-08-26 02:14:44 +0100

    Inital Commit (3)

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://fonts.googleapis.com/css?family=BenchNine|Dosis|Poppins:100,300&display=swap" rel="stylesheet">
+    <link href="JS/script.js">
+	<title>D'ANDR&Eacute; PHILLIPS | Portfolio </title>
+</head>
+<body>
+<div class="header">
+    <div class="container">
+        <h2 class="logo">D'André <span>Phillips</span></h2>
+    </div>
+</div>
+<div class="container-fluid welcome-screen">
+    <div class="row">
+        <div class="col-6 left-side">
+            <div class="text-area">
+                <h1 class="main-text">The <br><span>Portfolio.</span></h1>
+                <h2 class="sub-main-text">Freelance Web Developer</h2>
+            </div>
+        </div>
+        <div class="col-6 right-side">
+            <a class="xl-btn sweep-to-bottom" href="#">My Work ></a>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
