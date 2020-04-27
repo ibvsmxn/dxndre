94a185a81c5d02badcfe70dc3832f702d3bab46c
D'André Phillips <ibvsmxn@outlook.com>
2020-04-27 22:08:23 +0100

    Delete all

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = () => {
-    const target = document.body;
-    if(this.scrollY <= 1700) target.className = ''; else target.className = 'scroll';
-    if(this.scrollY > 4750) target.className = 'end';
-
-    console.log(this.scrollY);
-};
