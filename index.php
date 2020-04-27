635ccace66b9712908703e5cfc5230cdb9614e65
D'André Phillips <ibvsmxn@outlook.com>
2020-04-27 21:45:00 +0100

    Fresh look from Tia Rochelle

--- index.php
+++ index.php
@@ -1,44 +1,74 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php include("header.php"); ?>
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!-- Top Section -->
 
-    <link rel="stylesheet" href="CSS/style.css">
-    <link href="https://fonts.googleapis.com/css?family=BenchNine|Dosis|Poppins:100,300&display=swap" rel="stylesheet">
-    <link href="JS/script.js">
-	<title>D'ANDR&Eacute; PHILLIPS | Portfolio </title>
-</head>
-<body>
-<div class="header">
+<div class="jumbotron main-banner" style="background-image: url(images/dandre.jpg);">
+    <div class="content container">
+        <div class="logo-section">
+            <h1>D'André Phillips</h1>
+            <span class="tagline">Web Developer</span>
+        </div>
+    </div>
+</div>
+
+<!-- Intro Section -->
+
+<div class="jumbotron" id="bio">
+    <div class="content container description col-12 col-md-8 col-lg-6">
+
+        <h2>Bio</h2>
+
+        <h4>D'André is a Commercial and Freelance Front-End Web Developer.</h4>
+
+        <p>As a young girl, if somebody was to tell me that music would be the be-all and end-all of my life, I probably would’ve looked at you confused, chuckled embarrassingly and went back to playing “Mum’s and Dad’s” with my school friends. But here I am, in my early twenties living that exact reality.</p>
+        <p>I spent my teenage years battling with lack of self esteem, confidence and social anxiety, how am I supposed to do what I love that requires an attitude of the complete opposite?</p>
+        <p>At aged 14, I was passionately encouraged by my Mother to join a girl group in my local area; after 5 years, I went from being unable to publicly speak to singing more than talking.</p>
+        <p>Shows, festivals, studio time, photoshoots...you name it, I've been through it and I loved it. Every moment took me to the next stage of becoming the better version of myself, a Soul artist.</p>
+        <p>Calling all producers, artists and function venues, I’m your girl….lets collab!</p>
+
+    </div>
+</div>
+
+<!-- Websites Section -->
+
+<div class="website-section" id="websites">
     <div class="container">
-        <h2 class="logo">D'André <span>Phillips</span></h2>
+        <video width="100%" height="auto" controls>
+            <source src="videos/1.mp4" type="video/mp4">
+            Sorry, it looks like your browser does not support the video tag!
+            Please refresh the page or try viewing in a different browser.
+        </video>
+        <video width="100%" height="auto" controls>
+            <source src="videos/2.MOV" type="video/mp4">
+            Sorry, it looks like your browser does not support the video tag!
+            Please refresh the page or try viewing in a different browser.
+        </video>
+        <video width="100%" height="auto" controls>
+            <source src="videos/3.mov" type="video/mp4">
+            Sorry, it looks like your browser does not support the video tag!
+            Please refresh the page or try viewing in a different browser.
+        </video>
+        <video width="100%" height="auto" controls>
+            <source src="videos/4.MOV" type="video/mp4">
+            Sorry, it looks like your browser does not support the video tag!
+            Please refresh the page or try viewing in a different browser.
+        </video>
     </div>
 </div>
-<div class="container-fluid welcome-screen">
-    <div class="row">
-        <div class="col-6 left-side">
-            <div class="text-area">
-                <h1 class="main-text">The <br><span>Portfolio.</span></h1>
-                <h2 class="sub-main-text">Freelance Web Developer</h2>
-            </div>
-        </div>
-        <div class="col-6 right-side">
-            <a class="xl-btn sweep-to-bottom" href="#">My Work ></a>
-        </div>
-    </div>
 
+<!-- Contact Section -->
+
+<div class="contact-section" id="contact">
+    <div class="container jumbotron">
+        <h2>Contact</h2>
+
+        <ul class="socials">
+            <li><i class="far fa-envelope"></i><a href="mailto:dandrephillips@outlook.com?Subject=Getting%20In%20Touch" target="_top">dandrephillips@outlook.com</a></li>
+            <li><i class="fab fa-instagram"></i><a href="https://www.instagram.com/d.xndre/" target="_blank">@d.xndre_</a></li>
+            <li><i class="fab fa-linkedin"></i><a href="https://www.linkedin.com/in/dxndre" target="_blank">D'André Phillips</a></li>
+            <li><i class="fab fa-facebook-square"></i><a href="https://www.facebook.com/ibvsmxn" target="_blank">D'André Phillips</a></li>
+        </ul>
+    </div>
 </div>
 
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include("footer.php"); ?>
